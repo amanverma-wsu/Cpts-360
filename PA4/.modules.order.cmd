4dd5b13c74251ba6f36d1da80bab3bd5dffa42e3
User <user@example.com>
2026-03-17 04:01:35 -0700

    Remove build artifacts

--- PA4/.modules.order.cmd
+++
@@ -1,1 +0,0 @@
-savedcmd_modules.order := {   echo sysprog_counter.o; :; } > modules.order
--- PA4/.sysprog_counter.mod.cmd
+++
@@ -1,1 +0,0 @@
-savedcmd_sysprog_counter.mod := printf '%s\n'   sysprog_counter.o | awk '!x[$$0]++ { print("./"$$0) }' > sysprog_counter.mod
